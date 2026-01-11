369f709ca6c29e4df90a882761a0b132e215b74a
Your Name <you@example.com>
2026-01-11 21:29:33 +0530

    Added link to image of book on home page to dedicated book page

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import logo from "./assets/vbr-logo.png";
+import logo from "./assets/Logo.png";
 
 function Navbar() {
   return (
